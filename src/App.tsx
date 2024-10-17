2164d3cc6864bf5ed1d72930ca690f12c47561df
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-17 17:47:30 +0900

    feat: 투두 목록 보여줄 홈 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Layout from "./components/layout";
 import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/auth" />} />
       <Route path="/" element={<Layout />}>
-        <Route path="auth" element={<Home />} />
+        <Route path="auth" element={<Login />} />
         <Route path="auth/signup" element={<Signup />} />
+        <Route path="todos" element={<Home />} />
       </Route>
     </Routes>
   );
