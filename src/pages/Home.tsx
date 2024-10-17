2865fb23ad37f316bd14b00a65fd48136a26b459
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-17 16:24:42 +0900

    feat: 기존 폼을 shadcn 폼으로 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,64 +1,106 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useState } from "react";
+import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const Home = () => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
+  const formSchema = z.object({
+    email: z.string().email({
+      message: "이메일 형식이 올바르지 않습니다.",
+    }),
+    password: z.string().min(8, {
+      message: "비밀번호를 8자 이상 입력해주세요.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
   });
 
   const navigate = useNavigate();
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { id, value } = e.target;
-    setUser({
-      ...user,
-      [id]: value,
-    });
-  };
-
-  const onSubmit = () => {
-    alert("로그인 버튼 클릭");
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
   };
 
   const onClickSignup = () => {
     navigate("/auth/signup");
   };
+
   return (
-    <section className="flex flex-col gap-16 h-full py-20">
-      <h1 className="self-center text-4xl">PreOnboarding Todo</h1>
-      <form className="flex flex-col gap-10 px-4" onSubmit={onSubmit}>
-        <div className="flex flex-col gap-4">
-          <Label htmlFor="email">이메일</Label>
-          <Input
-            id="email"
-            type="email"
-            placeholder="이메일을 입력하세요."
-            value={user.email}
-            onChange={onChange}
-          />
-          <Label htmlFor="password">비밀번호</Label>
-          <Input
-            id="password"
-            type="password"
-            placeholder="비밀번호를 입력하세요."
-            value={user.password}
-            onChange={onChange}
-          />
-        </div>
-        <div className="flex gap-2">
-          <Button className="flex-1" onClick={onSubmit}>
-            로그인
-          </Button>
-          <Button className="flex-1" variant="outline" onClick={onClickSignup}>
-            회원가입
-          </Button>
-        </div>
-      </form>
-    </section>
+    <Form {...form}>
+      <section className="flex flex-col gap-16 h-full py-20 px-4">
+        <h1 className="self-center text-4xl">PreOnboarding Todo</h1>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-10"
+        >
+          <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>이메일</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="email"
+                      placeholder="이메일을 입력하세요."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>비밀번호</FormLabel>
+                  <FormControl>
+                    <Input
+                      id="password"
+                      type="password"
+                      placeholder="비밀번호를 입력하세요."
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex gap-2">
+            <Button className="flex-1" type="submit">
+              로그인
+            </Button>
+            <Button
+              className="flex-1"
+              variant="outline"
+              onClick={onClickSignup}
+            >
+              회원가입
+            </Button>
+          </div>
+        </form>
+      </section>
+    </Form>
   );
 };
 
