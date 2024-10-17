a552193f3ab2720ea652ee238d98ff0a66fffcaa
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-17 14:44:24 +0900

    chore: shadcn 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,19 @@
 import { defineConfig } from "vite";
+import path from "path";
 import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths(),
     svgr({
       include: "**/*.svg",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
