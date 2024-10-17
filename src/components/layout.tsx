c3c3ab190f6e938b88b5ac7247188eb3a64912fd
Gwang Soo <114225974+gs0428@users.noreply.github.com>
2024-10-17 17:53:37 +0900

    feat: 로그인인  상태와 아닐 때 네비게이팅 막기

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,21 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
   const queryClient = new QueryClient();
+  const isLogin = localStorage.getItem("token");
 
+  useEffect(() => {
+    if (isLogin && pathname.startsWith("/auth")) {
+      navigate("/todos");
+    } else if (!isLogin) {
+      navigate("/auth");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogin, pathname]);
   return (
     <QueryClientProvider client={queryClient}>
       <Outlet />
